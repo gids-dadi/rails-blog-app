0e62b091e7b6a939af3966280b3b34087b73a24f
gids-dadi <gideons564@gmail.com>
2022-09-24 00:10:20 +0100

    Make the suggested changes by the code reviwer

--- app/controllers/likes_controller.rb
+++
@@ -1,7 +0,0 @@
-class LikesController < ApplicationController
-  def update_like_counter(post)
-    likes = Like.where(posts_id: post)
-    l_posts = Post.find(post)
-    l_posts.update(LikesCounter: likes.length)
-  end
-end
--- app/controllers/users_controller.rb
+++
@@ -1,5 +0,0 @@
-class UsersController < ApplicationController
-  def last_3_post(int)
-    Post.where(author_id: int).last(3)
-  end
-end
